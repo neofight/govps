66aca1d39e006879b94bbc47282af36bbdf89045
William Hoggarth <mail@whoggarth.org>
2017-01-24 03:00:20 +0700

    addNginxFastCGIParameters now edits and reuploads the fastcgi_params file

--- nginx.go
+++ nginx.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
+	"strings"
 	"text/template"
 )
 
@@ -36,15 +38,49 @@ type addNginxFastCGIParameters struct {
 
 func (step addNginxFastCGIParameters) Execute(cxt context) error {
 
-	data, err := scpDownload(cxt.Client, "/etc/nginx/fastcgi_params")
+	file, err := scpDownload(cxt.Client, "/etc/nginx/fastcgi_params")
 
 	if err != nil {
 		return fmt.Errorf("Failed to download file: %v", err)
 	}
 
-	fmt.Print(data)
+	scanner := bufio.NewScanner(strings.NewReader(file))
 
-	return nil
+	pathInfoSet := false
+	scriptFilenameSet := false
+
+	for scanner.Scan() {
+		parts := strings.Fields(scanner.Text())
+
+		if len(parts) == 3 && parts[0] == "fastcgi_param" {
+			switch parts[1] {
+			case "PATH_INFO":
+				pathInfoSet = true
+			case "SCRIPT_FILENAME":
+				scriptFilenameSet = true
+			}
+		}
+	}
+
+	if !pathInfoSet {
+		file += "\nfastcgi_param  PATH_INFO          \"\""
+		fmt.Println("Added PATH_INFO to fastcgi_params")
+	} else {
+		fmt.Println("PATH_INFO already present in fastcgi_params")
+	}
+
+	if !scriptFilenameSet {
+		file += "\nfastcgi_param  SCRIPT_FILENAME    $document_root$fastcgi_script_name;"
+		fmt.Println("Added SCRIPT_FILENAME to fastcgi_params")
+	} else {
+		fmt.Println("SCRIPT_FILENAME already present in fastcgi_params")
+	}
+
+	if pathInfoSet && scriptFilenameSet {
+		return nil
+	} else {
+		return scpUploadAsRoot(cxt.Client, file, "/etc/nginx/fastcgi_params", cxt.password)
+	}
 }
 
 var nginxTemplate = `server {
