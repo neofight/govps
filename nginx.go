80d1cda819944f1b5f1b546593d090535e4bca9d
William Hoggarth <mail@whoggarth.org>
2017-01-31 17:39:49 +0700

    Improved error handling for Nginx step

--- nginx.go
+++ nginx.go
@@ -53,25 +53,34 @@ func (step addNginxFastCGIParameters) Execute(cxt context) error {
 		}
 	}
 
+	successMessage := ""
+
 	if !pathInfoSet {
 		file += "\nfastcgi_param  PATH_INFO          \"\";"
-		fmt.Println("Added PATH_INFO to fastcgi_params")
+		successMessage += "Added PATH_INFO to fastcgi_params\n"
 	} else {
-		fmt.Println("PATH_INFO already present in fastcgi_params")
+		successMessage += "PATH_INFO already present in fastcgi_params\n"
 	}
 
 	if !scriptFilenameSet {
 		file += "\nfastcgi_param  SCRIPT_FILENAME    $document_root$fastcgi_script_name;"
-		fmt.Println("Added SCRIPT_FILENAME to fastcgi_params")
+		successMessage += "Added SCRIPT_FILENAME to fastcgi_params\n"
 	} else {
-		fmt.Println("SCRIPT_FILENAME already present in fastcgi_params")
+		successMessage += "SCRIPT_FILENAME already present in fastcgi_params\n"
 	}
 
-	if pathInfoSet && scriptFilenameSet {
-		return nil
-	} else {
-		return ssh.ScpUploadDataAsRoot(cxt.Client, file, "/etc/nginx/fastcgi_params", cxt.password)
+	if !pathInfoSet || !scriptFilenameSet {
+
+		err = ssh.ScpUploadDataAsRoot(cxt.Client, file, "/etc/nginx/fastcgi_params", cxt.password)
+
+		if err != nil {
+			return fmt.Errorf("Failed to update fastcgi_params: %v", err)
+		}
 	}
+
+	fmt.Print(successMessage)
+
+	return nil
 }
 
 var nginxTemplate = `server {
